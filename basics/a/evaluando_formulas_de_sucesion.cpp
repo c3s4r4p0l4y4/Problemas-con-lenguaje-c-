0e5894ae29077ca09da1acda17d3ef89ad08eed4
c3s4r4p0l4y4 <88946719+c3s4r4p0l4y4@users.noreply.github.com>
2021-08-17 10:51:59 -0500

    Add files via upload

---
+++ basics/a/evaluando_formulas_de_sucesion.cpp
@@ -0,0 +1,31 @@
+﻿/***
+*   author: cesar
+*   created: 07.06.2021 21:38:24
+***/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    //solucion a un problema, con c++
+
+    double y1,y2,y,z1,z2,z,x;
+    cin>>x;
+
+    y1=x+5;
+    y2=2*(x+1);
+    y=y1/y2;
+
+    z1=pow(y,2)+x*(x-2*y);
+    z2=x*y;
+    z=z1/z2;
+
+    cout<<z;
+
+    return 0;
+}
